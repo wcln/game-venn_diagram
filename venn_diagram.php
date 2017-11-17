5ba414c5f0a5ceb757d8e093cb69496cd6d167d8
Colin Bernard <colinjbernard@hotmail.com>
2017-11-17 11:17:33 -0800

    now reads version info from json file

--- venn_diagram.php
+++ venn_diagram.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>BCLN - Divisibility</title>
+	<title>BCLN - <?=$_GET['title']?></title>
 	<meta charset="utf-8"/>
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato"><!-- google web font -->
 	<link rel="stylesheet" type="text/css" href="style/style.css"/>
@@ -9,20 +9,26 @@
 	<script src="https://bclearningnetwork.com/lib/jquery/jquery-3.1.0.min.js"></script>
 	<script src="https://bclearningnetwork.com/lib/createjs/createjs-2015.11.26.min.js"></script><!-- CreateJS library hosted on BCLN server -->
 	<script src="lib/ndgmr.Collision.js"></script>
-	<script type="text/javascript" src="helper.js"></script><!-- contains helper functions which do not call functions in balloon.js -->
-	<script type="text/javascript" src="venn_diagram.js"></script><!-- the main game JS file -->
 
 	<script>
 		var questions = [];
-		var customStartText = "Test text pls ignore";
-		var questionsPerLevel = 10;
+		var questionsPerLevel;
+		var customStartText;
 
-		$.getJSON("versions/<?=_GET['title']?>.json", function(json) {
-				// TODO: initialize the above variables
+		$.getJSON("versions/<?=$_GET['title']?>/questions.json", function(questions_json) {
+				questions = questions_json;
 		});
+		$.getJSON("versions/<?=$_GET['title']?>/data.json", function(data_json) {
+				customStartText = data_json[0].startText;
+				questionsPerLevel = data_json[0].questionsPerLevel;
+		});
+
 	</script>
 
 
+
+	<script type="text/javascript" src="helper.js"></script><!-- contains helper functions which do not call functions in balloon.js -->
+	<script type="text/javascript" src="venn_diagram.js"></script><!-- the main game JS file -->
 </head>
 <body onload="init();"><!-- body onload calls function to initialize game -->
 
