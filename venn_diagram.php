c628b76a7de0eeec03d1b40ff8809cd904eae440
Colin Bernard <colinjbernard@hotmail.com>
2017-12-20 17:07:40 -0800

    update jquery version

--- venn_diagram.php
+++ venn_diagram.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato"><!-- google web font -->
 	<link rel="stylesheet" type="text/css" href="style/style.css"/>
 	<link rel="shortcut icon" href="images/favicon.ico"/>
-	<script src="https://bclearningnetwork.com/lib/jquery/jquery-3.1.0.min.js"></script>
+	<script src="https://bclearningnetwork.com/lib/jquery/jquery-3.2.1.min.js"></script>
 	<script src="https://bclearningnetwork.com/lib/createjs/createjs-2015.11.26.min.js"></script><!-- CreateJS library hosted on BCLN server -->
 	<script src="lib/ndgmr.Collision.js"></script>
 
